63e00f122bd3a3c4c4b9e9732aa658eb94350abd
CodeVANIE <kungfuninja302@gmail.com>
2025-09-19 20:38:04 +0800

    Delete jpg resume, fix todo link

--- src/pages/Projects/ProjectTodoTile.jsx
+++ src/pages/Projects/ProjectTodoTile.jsx
@@ -5,7 +5,7 @@ export default function ProjectTodoTile() {
     const { pathname } = useLocation();
     
     return (
-        <a href="https://todo-to-do.vercel.app/" target="_blank" rel="noopener noreferrer" className={`project-tiles bg-black/50 bg-blend-multiply bg-todotodo bg-cover font-quicks shadow-[2px_0_8px_#e7b574,-2px_0_8px_#e7b574] ${pathname === "/home" ? "max-w-[275px]" : ""}`}>
+        <a href="https://vanie-todo-to-do.vercel.app/" target="_blank" rel="noopener noreferrer" className={`project-tiles bg-black/50 bg-blend-multiply bg-todotodo bg-cover font-quicks shadow-[2px_0_8px_#e7b574,-2px_0_8px_#e7b574] ${pathname === "/home" ? "max-w-[275px]" : ""}`}>
             <div className="w-full grid place-items-center">
                 <img src={todoicon} alt="todoicon" className="w-20"/>
                 <h1 className='text-3xl font-extrabold text-ptlbrown-100 text-shadow-sm text-shadow-yellow-950'>TODO-To-Do</h1>
